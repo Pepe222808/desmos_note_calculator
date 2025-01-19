8b69128246d6616084a32d13c07827c49874eb6d
Pepe222808 <129376609+Pepe222808@users.noreply.github.com>
2025-01-19 23:09:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# **Note Calculator for Desmos**
+
+## Description
+A calculator that takes music notes in letter notation (both English and German) and creates a range of notes along with a list of sounds to copy and paste into Desmos. 
+By using Desmos' tone function, it can play the song using the frequencies provided by the calculator. 
+Inspired by seeing various songs created in Desmos as a fun side project, I decided to create my own. To make the process easier, I wrote a script that generates the songs for me. Once I finished, I thought it would be a great idea to share it with my friends and others who might also want to create songs in Desmos but don't want to spend countless hours learning how to do it.
+
+## Technologies:
+<div><img src='https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/python.png' width='100px' height='100px' style='float:left;margin-right:50px'></img></div>
+<div><a href='https://www.desmos.com/calculator'><img src='https://play-lh.googleusercontent.com/D9R7uT-u64HzMur04xJZJEaALJwIHUXLMMIFL0hp1351-eQLCzqc9s7i6xbkj6f6Bw=w416-h235-rw' width='200px' height='100px'></img></a></div>
+Built with Python 3.13
+
+## HOW TO
+To use the program, you need to have Python installed. If you need help installing Python, here is a guide on how to do it:
+https://realpython.com/installing-python/#windows-how-to-install-python-using-the-official-installer
+
+After installing Python, move the program to your Python folder and launch it using the `run.bat` file.
+
+#### Calculator
+
+1. Enter the lowest note used in the song (e.g., c2).
+!(/assets/screen1.png)
+2. Enter the highest note used in the song (e.g., c4).
+!(/assets/screen2.png)
+3. The definition of different note frequencies from the given range will be generated as a list, which you will paste into Desmos. This process is further explained in the Desmos tutorial below.
+4. Manually enter the notes separated by commas into the program. Add pauses between notes to ensure they sound correctly. (A pause is always added automatically after each note to make the change in sound clear.)
+5. Once you have finished entering the notes, type `0` to end the program.
+!(/assets/screen3.png)
+6. Paste the lines into Desmos as explained in the tutorial below.
+
+#### Desmos
+
+*The order in which you paste lines into Desmos does **not** matter.*
+1. Copy the output line by line into Desmos as shown below:
+
+ > **WARNING! Note frequency definitions (`f(x) = {...}`) may paste the braces in incorrect positions (or may not paste them at all). You will need to fix this manually by adding the opening brace `{` after the `=` sign, the closing brace `}` at the end of the entire list, and removing the last comma in the list before the closing brace.**
+ !(/assets/screen4.png)
+ !(/assets/screen5.png)
+
+ !(/assets/screen6.png)
+ !(/assets/screen7.png)
+2. Create the variable sliders suggested by Desmos (click on the small icons to add sliders).
+!(/assets/screen8.png)
+3. To set up the song correctly, adjust the speed and the range of the variable sliders. The number after the 'length of smaller segments' represents the maximum value. The minimum value for the first slider should be set to 1, and for the next sliders, it should be set to 0.
+!(/assets/screen9.png)
+!(/assets/screen10.png)
+4. To adjust the speed, open the slider settings (the icon under the 'play button') and reduce the speed to match your song's tempo. Change the animation mode to 'Play Once.'
+!(/assets/screen11.png)
+5. Enable sound at the top of the page by clicking the speaker icon.
+!(/assets/screen12.png)
+
+**If you have more sliders it will look like this:**
+!(/assets/screen13.png)
+
+After finishing all the steps, hit play on the first slider and when it ends, start the next one and so on. If the song does not sound right, try editing some settings or adding more pauses in the program.
+Have fun making your songs and show off to your friends and family that you can do math :)).
+
---
+++ note_calculator.py
@@ -0,0 +1,197 @@
+import math
+
+class note:
+    baseFrequency=0
+    octave=0
+    letter=0
+    length=0
+    def __init__(self, o):
+        self.octave = o
+    def calculate(self, x):
+        frequency = self.baseFrequency*(2**self.octave)
+        length = round(1000/frequency, 3)
+        global dl
+        dl = round(length, 3)
+        df = round(frequency, 3)
+        output = f"{x}<x<={round(dl+x,3)}:{df},"
+        notes_range.append(output)
+        return output
+        
+class C(note):
+    baseFrequency = 16.35
+    letter = "C"  
+class CHash(note):
+    baseFrequency = 17.32
+    letter = "C#"
+class D(note):
+    baseFrequency = 18.35
+    letter = "D" 
+class DHash(note):
+    baseFrequency = 19.45
+    letter = "D#"
+class E(note):
+    baseFrequency = 20.6
+    letter = "E" 
+class F(note):
+    baseFrequency = 21.83
+    letter = "F"  
+class FHash(note):
+    baseFrequency = 23.12
+    letter = "F#"  
+class G(note):
+    baseFrequency = 24.5
+    letter = "G"
+class GHash(note):
+    baseFrequency = 25.96
+    letter = "G#"
+class A(note):
+    baseFrequency = 27.5
+    letter = "A"
+class AHash(note):
+    baseFrequency = 29.14
+    letter = "A#"
+class B(note):
+    baseFrequency = 30.87
+    letter = "B"
+ 
+def desmos_note(sound,x_value): 
+    octave = int(sound[-1])
+    note_sign = sound[:-1]
+    print(note_sign)
+    if (note_sign == "c"):
+        c = C(octave)
+        notes.update({note_sign+str(octave) : c.calculate(x_value)})
+        return "c#" + str(octave)
+    elif (note_sign == "c#"):
+        cH = CHash(octave)
+        notes.update({note_sign+str(octave) : cH.calculate(x_value)})
+        return "d" + str(octave)
+    elif (note_sign == "d"):
+        d = D(octave)
+        notes.update({note_sign+str(octave) : d.calculate(x_value)})
+        return "d#" + str(octave)
+    elif (note_sign == "d#"):
+        dH = DHash(octave)
+        notes.update({note_sign+str(octave) : dH.calculate(x_value)})
+        return "e" + str(octave)
+    elif (note_sign == "e"):
+        e = E(octave)
+        notes.update({note_sign+str(octave) : e.calculate(x_value)})
+        return "f" + str(octave)
+    elif (note_sign == "f"):
+        f = F(octave)
+        notes.update({note_sign+str(octave) : f.calculate(x_value)})
+        return "f#" + str(octave)
+    elif (note_sign == "f#"):
+        fH = FHash(octave)
+        notes.update({note_sign+str(octave) : fH.calculate(x_value)})
+        return "g" + str(octave)
+    elif (note_sign == "g"):
+        g = G(octave)
+        notes.update({note_sign+str(octave) : g.calculate(x_value)})
+        return "g#" + str(octave)
+    elif (note_sign == "g#"):
+        gH = GHash(octave)
+        notes.update({note_sign+str(octave) : gH.calculate(x_value)})
+        return "a" + str(octave)
+    elif (note_sign == "a"):
+        a = A(octave)
+        notes.update({note_sign+str(octave) : a.calculate(x_value)})
+        return "a#" + str(octave)
+    elif (note_sign == "a#"):
+        aH = AHash(octave)
+        notes.update({note_sign+str(octave) : aH.calculate(x_value)})
+        return "b" + str(octave)
+    elif (note_sign == "h" or note_sign =="b"):
+        b = B(octave)
+        notes.update({note_sign+str(octave) : b.calculate(x_value)})
+        return "c" + str((octave+1))
+    return 
+  
+def prep_notes(start,end):
+    x=0
+    global notes, notes_range
+    notes = {}
+    notes_range = ["0=x:0,"]
+    
+    while start != end:
+        start = desmos_note(start,x)
+        x=round(x+dl, 3)
+    print(" ")
+    # print("zakres nut >>")
+    # print(notes)
+    print("desmos input >>")
+    print("Definition of diffrent notes frequencies from given range:   f(x) = {",end="")
+    for a in range(len(notes_range)):
+        print(f"{notes_range[a].strip("'")}",sep=",",end=" ")
+        a+=1
+    print("}",end="")
+    print(" ")
+          
+def song_maker():
+    print(" ")
+    print("NOTE: THIS CALCULATOR TAKES NOTES IN LETTER NOTATION AND WITH THE OCTAVE NUMBER!!!")
+    print("example: c2, e6, a#4, f#1")
+    print(" ")
+    start = input("""enter the start of the note ferquencies definition (the lowest used sound in the song):
+    """)
+    if start[:-1] == "h":
+        octave = start[-1]
+        start = "b" + octave 
+    start = start.lower()
+    end = input("""enter the note after the highest note used in the song:
+    """)
+    if end[:-1] == "h":
+        octave = end[-1]
+        end = "b" + octave 
+    end = end.lower()
+    prep_notes(start,end)  
+    notes_output = [0]
+    while 1:
+        _input = input(
+    """enter one or multiple notes separated with commas (',')
+    -if u want to add additional brakes in between the sounds enter 'p'
+    -if u want to end the input enter '0':
+    """)
+        _input = _input.lower()
+        list = _input.split(",")
+        for element in list:
+            if element == "p":
+                notes_output.append(0)
+        
+            if element[:-1] == "h":
+                octave = element[-1]
+                element = "b" + octave 
+
+            klucze = notes.keys()
+            for k in klucze:
+                if element == k: 
+                    notes_output.append(float(notes[k][(notes[k].find("<="))+2:notes[k].find(":")]))
+                    notes_output.append(0)
+                    break
+        if element == "0" or list[-1] == "0":
+            break
+    print(" ")
+    print(f"List of note definitions:   a={notes_output}")
+    a = ""
+    x = len(notes_output)/300
+    range_length = len(notes_output)/math.ceil(x)
+    for i in range(math.ceil(x)):
+        a += chr(105+i)
+        if (i!= math.ceil(x)-1):
+            a += "+"
+    print(f"List of iterators:   a[{a}]")
+    print(f"Tone function:   tone(f(a[{a}]))")  
+    print(f"Total lenght of the list with notes:   {len(notes_output)} ")
+    print(f"Length of smaller segments for iterator ranges:   {math.ceil(range_length)}")      
+    print(" ")
+  
+  
+song_maker() 
+
+
+x = input("Hit Enter to Exit, or Input any other key to continue ")
+
+if not x :
+    print("Exiting the Program.")
+    exit()
